27bfae296584a37974950d92af99ce2fba67ad17
Kohnodker Khademul Bari <bari35-1159@diu.edu.bd>
2021-08-19 18:37:16 +0600

    Added Some component

--- src/App.js
+++ src/App.js
@@ -2,15 +2,53 @@
 import './App.css';
 import Header from './components/Header/Header';
 import {
-  BrowserRouter as Router, Switch,
+  BrowserRouter as Router, Route, Switch,
 } from "react-router-dom";
 import Home from './components/Home/Home';
+import NotFound from './components/NotFound/NotFound';
+import Salad from './components/Salad/Salad';
+import Pizza from './components/Pizza/Pizza';
+import Steak from './components/Steak/Steak';
+import Drink from './components/Drink/Drink';
+import Burger from './components/Burger/Burger';
+import Login from './components/Login/Login';
+
 function App() {
   return (
     <Router>
       <Header></Header>
       <Switch>
-        
+        <Route exact path='/'>
+          <Home></Home>
+        </Route>
+        <Route path='/burger'>
+          <Burger></Burger>
+        </Route>
+
+        <Route path='/salad'>
+          <Salad></Salad>
+        </Route>
+
+        <Route path='/pizza'>
+          <Pizza></Pizza>
+        </Route>
+
+        <Route path='/steaks'>
+          <Steak></Steak>
+        </Route>
+
+        <Route path='/drinks'>
+          <Drink></Drink>
+        </Route>
+
+        <Route path="/login">
+          <Login/>
+        </Route>
+
+        <Route path="*">
+          <NotFound/>
+        </Route>
+
       </Switch>
 
     </Router>
---
+++ src/components/Drink/Drink.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Drink = () => {
+    return (
+        <div>
+            <h1>this is from drink</h1>
+        </div>
+    );
+};
+
+export default Drink;
